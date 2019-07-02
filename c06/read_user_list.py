16aafe231c3c53f5cf5461ea15c1b70c8e20f35a
al17056SK000 <al17056@shibaura-it.ac.jp>
2019-07-02 13:34:53 +0900

    新たに作ったw18.htmlに遷移するときに使う　read_user_list を追加しました

---
+++ c06/read_user_list.py
@@ -0,0 +1,33 @@
+# 新たに作ったw18.htmlに遷移するときに使う
+import mysql.connector
+from .connect_report_db import connect_report_db, close_db
+import logging
+
+read_user_list_who_had_submitted = (
+    "SELECT userID from test_table GROUP BY userID "
+)
+
+
+def read_user_list():
+
+    cnx = connect_report_db()
+    if cnx is None:
+        logging.error("failed get connector")
+        return list()
+
+    try:
+        cursor = cnx.cursor()
+        cursor.execute(read_user_list)
+        result = cursor.fetchall()
+        logging.info("get user list from db is success")
+
+        cursor.close()
+        cnx.close()
+        logging.info("close connector")
+    except mysql.connector.Error as err:
+        logging.error("failed get user list from db")
+        logging.error(err)
+        close_db(cursor, cnx)
+        return list()
+
+    return result
